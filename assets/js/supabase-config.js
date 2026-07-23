a9969c0a1a3c50c6f205c2a694ad1f929fca8636
mastrninebo <kasangula78@yahoo.com>
2026-07-23 12:20:39 +0200

    Supabase-II

---
+++ assets/js/supabase-config.js
@@ -0,0 +1,5 @@
+// assets/js/supabase-config.js
+// THIS FILE IS GITIGNORED - Contains real keys
+
+export const SUPABASE_URL = 'https://hrxudtmskbukikucufqw.supabase.co';
+export const SUPABASE_ANON_KEY = 'sb_publishable_0XXmjurmrLAp3aTo0OfBng_sxJg82Vl';
